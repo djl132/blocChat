5449114ee19d3e9651dba36971a567d68aafce59
djl132 <djl132@case.edu>
2017-01-27 13:59:00 -0800

    Added Grunt and npmInstall

--- rooms.js
+++ rooms.js
@@ -1,7 +1,7 @@
 
 //A Factory Service that exposes backend room information to eh chatController managing the chat app's
 (function() {
-  function Room(firebase,$fireBaseArray){
+  function Room($fireBaseArray){
     var ref = firebase.database().ref().child('rooms'); //get json array of rooms aka accessing rooms array in the root database json object.
     var rooms = $firebaseArray(ref);//gets jsarray-form of rooms array
     
